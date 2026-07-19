8a66172d428d4291c1f2567ecab7a8ba403faeef
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-07-19 14:22:17 +0530

    Update solution.R

--- solution.R
+++ solution.R
@@ -1,19 +1,17 @@
-
-# Program 1 – Data Visualization using ggplot2
-
 library(ggplot2)
 
-# The mpg dataset is loaded automatically with ggplot2.[web:10]
+plot1 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(se = FALSE)
 
-# TODO: Create the four plots exactly as specified.
+plot2 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(aes(group = drv), se = FALSE)
 
-# Plot 1: scatter + smooth (overall)
+plot3 <- ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  geom_smooth(se = FALSE)
 
-
-# Plot 2: scatter + smooth grouped by drv
-
-
-# Plot 3: scatter colored by drv + smooth overall
-
-
-# Plot 4: scatter colored by drv + smooth overall (color only in points)
+plot4 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  geom_smooth(se = FALSE)
